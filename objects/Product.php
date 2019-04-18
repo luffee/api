334a2e626a4f4f80bb0ec1d7b7f4eb26b1b26105
Nenad Anđelković <badnenada@googlemail.com>
2019-04-19 01:25:25 +0200

    initial commit

---
+++ objects/Product.php
@@ -0,0 +1,64 @@
+<?php
+
+
+class Product
+{
+    // database connection and table name
+    private $conn;
+
+
+    // object properties
+    public $id;
+    public $name;
+    public $price;
+    public $sku;
+
+    // constructor with $db as database connection
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function read()
+    {
+        $query = "SELECT vm_p.virtuemart_product_id AS id, product_sku AS sku, product_name AS name, product_price AS price
+                    FROM b635h_virtuemart_products vm_p, b635h_virtuemart_products_sr_yu vm_p_sr, b635h_virtuemart_product_prices vm_pr
+                    WHERE 
+                    vm_p.virtuemart_product_id= vm_p_sr.virtuemart_product_id
+                    AND 
+                    vm_p_sr.virtuemart_product_id = vm_pr.virtuemart_product_id";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+
+
+    }
+
+    public function search($keyword)
+    {
+        $query = "SELECT vm_p.virtuemart_product_id AS id, product_sku AS sku, product_name AS name, product_price AS price
+                    FROM b635h_virtuemart_products vm_p, b635h_virtuemart_products_sr_yu vm_p_sr, b635h_virtuemart_product_prices vm_pr
+                    WHERE
+                    vm_p.virtuemart_product_id= vm_p_sr.virtuemart_product_id
+                    AND
+                    vm_p_sr.virtuemart_product_id = vm_pr.virtuemart_product_id
+                    AND  vm_p.product_sku LIKE ?";
+
+        $stmt = $this->conn->prepare($query);
+
+        $keyword = htmlspecialchars(strip_tags($keyword));
+        $keyword = "%{$keyword}%";
+
+        //bind
+        $stmt->bindParam(1, $keyword);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+
+}
---
+++ product/read.php
@@ -0,0 +1,45 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/dbcon.php';
+include_once '../objects/Product.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$product = new Product($db);
+
+$stmt = $product->read();
+$num = $stmt->rowCount();
+
+if ($num > 0) {
+    $products_arr = array();
+    $products_arr["records"] = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+        extract($row);
+
+        $product_item = array(
+            "id" => $id,
+            "name" => $name,
+            "sku" => $sku,
+            "price" => $price
+        );
+        array_push($products_arr["records"], $product_item);
+    }
+
+    http_response_code(200);
+
+    echo json_encode($products_arr);
+} else {
+
+    http_response_code(404);
+
+    echo json_encode(
+        array("message" => "No products found.")
+    );
+}
